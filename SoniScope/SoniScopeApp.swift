836fc38703cb9df0a0dfdc9a115b966e91c2b36d
25ChilingH <25ChilingH@students.harker.org>
2025-07-14 18:03:49 -0700

    Initial commit

---
+++ SoniScope/SoniScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoniScopeApp.swift
+//  SoniScope
+//
+//  Created by Chiling Han on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct SoniScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SoniScopeTests/SoniScopeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SoniScopeTests.swift
+//  SoniScopeTests
+//
+//  Created by Chiling Han on 7/14/25.
+//
+
+import Testing
+@testable import SoniScope
+
+struct SoniScopeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
