7af166ebf3f5eb3c6ee8e771def6c321527859f3
Ace-Kat <92191466+Ace-Kat@users.noreply.github.com>
2025-07-14 21:53:37 -0700

    Added inital Bluetooth functionality

---
+++ SoniScope/AccessoryModel.swift
@@ -0,0 +1,37 @@
+//
+//  AccessoryModel.swift
+//  SoniScope
+//
+//  Created by Venkata Siva Ramisetty on 7/14/25.
+//
+import Foundation
+import CoreBluetooth
+import UIKit
+
+enum AccessoryModel {
+    case soniScope
+    
+    var accessoryName: String {
+        switch self {
+            case .soniScope: "Respify SoniScope"
+        }
+    }
+    
+    var displayName: String {
+        switch self {
+            case .soniScope: "SoniScope"
+        }
+    }
+    
+    var serviceUUID: CBUUID {
+        switch self {
+            case .soniScope: CBUUID(string: "")
+        }
+    }
+    
+    var accessoryImage: UIImage {
+        switch self {
+        case .soniScope:
+        }
+    }
+}
